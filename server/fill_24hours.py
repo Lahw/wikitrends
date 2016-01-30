33a940a28a3e3f4a77d909d469d3838d58363eb9
Yann <yann.carbonne@gmail.com>
2016-01-30 16:52:12 +0100

    CHORE: avoid variable reassignment #thankslucas

--- server/fill_24hours.py
+++ server/fill_24hours.py
@@ -18,19 +18,12 @@ rdd = sc.union([proc(f) for f in glob.glob("/mnt/wikidata/wikistats/*")])
 ##############
 ## Compute sum
 ##############
-# group by name / month / day
-rdd_24hours = rdd.map(lambda x: (x['name'] + ' ' + x['date_time'][:10], x['n_view']))
-# sum the n_view
-rdd_24hours = rdd_24hours.reduceByKey(lambda a, b: a + b)
-# rebuild
-rdd_24hours_sum = rdd_24hours.map(lambda x: {'name': x[0].split(' ')[0], 'date_time': x[0].split(' ')[1], 'n_view': x[1]})
-
-############################
-## Get n_view for each hour
-############################
-# same logic as before
-rdd_24hours_hours = rdd.map(lambda x: (x['name'] + ' ' + x['date_time'][:10], x['date_time'][11:13] + ' ' + str(x['n_view'])))
-rdd_24hours_hours = rdd_24hours_hours.reduceByKey(lambda a, b: a + ',' + b)
+# 1. group by name / month / day
+# 2. sum the n_view
+# 3. rebuild
+rdd_24hours_sum = rdd.map(lambda x: (x['name'] + ' ' + x['date_time'][:10], x['n_view'])) \
+                     .reduceByKey(lambda a, b: a + b) \
+                     .map(lambda x: {'name': x[0].split(' ')[0], 'date_time': x[0].split(' ')[1], 'n_view': x[1]})
 
 
 def create_entry(x):
@@ -47,8 +40,13 @@ def create_entry(x):
         if key_name not in res_dict.keys():
             res_dict[key_name] = 0
     return res_dict
-
-rdd_24hours_hours = rdd_24hours_hours.map(lambda x: create_entry(x))
+############################
+## Get n_view for each hour
+############################
+# same logic as before
+rdd_24hours_hours = rdd.map(lambda x: (x['name'] + ' ' + x['date_time'][:10], x['date_time'][11:13] + ' ' + str(x['n_view']))) \
+                       .reduceByKey(lambda a, b: a + ',' + b) \
+                       .map(lambda x: create_entry(x))
 
 
 ############################
@@ -63,5 +61,6 @@ df_24hours = df_24hours_sum.join(df_24hours_hours, (df_24hours_sum['name'] == df
 #############################
 ## Format & Save to Cassandra
 #############################
+# ugly but didn't find anything better
 rdd_24hours = df_24hours.map(lambda x: {'date_time': x[0], 'n_view': x[1], 'name': x[2], 'h00': x[3], 'h01': x[4], 'h02': x[5], 'h03': x[6], 'h04': x[7], 'h05': x[8], 'h06': x[9], 'h07': x[10], 'h08': x[11], 'h09': x[12], 'h10': x[13], 'h11': x[14], 'h12': x[15], 'h13': x[16], 'h14': x[17], 'h15': x[18], 'h16': x[19], 'h17': x[20], 'h18': x[21], 'h19': x[22], 'h20': x[23], 'h21': x[24], 'h22': x[25], 'h23': x[26]})
 rdd_24hours.saveToCassandra('wiki', 'trends_24hours')
