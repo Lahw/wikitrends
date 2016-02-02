bb23639876cf2c1756dce3f70d43686f3e00fbf1
Yann <yann.carbonne@gmail.com>
2016-02-02 23:14:42 +0100

    FEAT: 30days for client

--- client/application.py
+++ client/application.py
@@ -6,21 +6,22 @@ import csv
 import re
 application = Flask(__name__)
 
+
 def cleaner(name):
-    black_list = ['Main_Page','404_error', 'Special:Random' , 'Wikipedia:Hauptseite','Wikipedia:Portada','Special:AutoLogin','Spezial:Suche',
-                  'Spezial:Search','Portal:Huvudsida','Spezial:Zuf%C3%A4llige_Seite','index.html','Wikipedia:P%C3%A1gina_principal','w/index.php',
-                  'Special:Watchlist','Special:Search','Main20Page','Pagina_principale','A9diaAccueil_principal','Hauptseite','Hoofdpagina','C3B3wna',
-                  'C3A1gina_principal','Accueil_principal','P%C3%A1gina_principal','Strona_g%C5%82%C3%B3wna','Search'] 
+    black_list = ['Main_Page', '404_error', 'Special:Random', 'Wikipedia:Hauptseite', 'Wikipedia:Portada', 'Special:AutoLogin', 'Spezial:Suche',
+                  'Spezial:Search', 'Portal:Huvudsida', 'Spezial:Zuf%C3%A4llige_Seite', 'index.html', 'Wikipedia:P%C3%A1gina_principal', 'w/index.php',
+                  'Special:Watchlist', 'Special:Search', 'Main20Page', 'Pagina_principale', 'A9diaAccueil_principal', 'Hauptseite', 'Hoofdpagina', 'C3B3wna',
+                  'C3A1gina_principal', 'Accueil_principal', 'P%C3%A1gina_principal', 'Strona_g%C5%82%C3%B3wna', 'Search']
 
     min_length = 3
     pattern = re.compile(r'(%[A-Z0-9][A-Z0-9]){1}')
-    if name in black_list or len(name) < min_length or pattern.match(name) :
+    if name in black_list or len(name) < min_length or pattern.match(name):
         return None
-    name=re.split(':|/',name)
-    if len(name)<=3:
-        name=name[-1]
+    name = re.split(':|/', name)
+    if len(name) <= 3:
+        name = name[-1]
     else:
-        name=name[-2]+name[-1]
+        name = name[-2] + name[-1]
     if name in black_list or pattern.match(name):
         return None
     return name
@@ -38,79 +39,64 @@ nb_pages = 10
 @application.route('/index')
 def index():
     path_to_data = 'static/data/'
+    default_date = 'Exemple: 2011-01-31'
     if request.args.get('date'):
         date_string = str(request.args.get('date'))
+        default_date = date_string
+        for mode in ['24hours', '30days']:
+            if mode == '24hours':
+                prefix = 'h'
+            else:
+                prefix = 'd'
+            dict_piechart = []
+            dict_linechart = []
+            query = "SELECT * FROM wiki.trends_" + mode + " WHERE date_time = '" + date_string + \
+                    "' ORDER BY n_view DESC LIMIT 50;"
+            raws = session.execute(query)
+            sum_view = 0.
+            sum_view_per_hour = {}
+            cpt = 0
+            for raw in raws:
+                if cpt >= nb_pages:
+                    break
+                clean_name = cleaner(raw['name'])
+                if clean_name is not None:
+                    dict_piechart.append({'category': clean_name, 'measure': int(raw['n_view'])})
+                    range_indices = range(24) if mode == '24hours' else range(30)
+                    for indice in range_indices:
+                        indice_string = str(indice) if indice >= 10 else "0" + str(indice)
+                        dict_linechart.append({'category': int(indice_string), 'group': clean_name, 'measure': int(raw[prefix + indice_string])})
+                        if not prefix + indice_string in sum_view_per_hour.keys():
+                            sum_view_per_hour[prefix + indice_string] = int(raw[prefix + indice_string])
+                        else:
+                            sum_view_per_hour[prefix + indice_string] += int(raw[prefix + indice_string])
+                    sum_view += raw['n_view']
+                    cpt += 1
 
-        ##########
-        # 24 hours
-        ##########
-        dict24hours_piechart = []
-        dict24hours_linechart = []
-        query = "SELECT * FROM wiki.trends_24hours WHERE date_time = '" + date_string + \
-                "' ORDER BY n_view DESC LIMIT 50;"
-        raws = session.execute(query)
-        sum_view = 0.
-        sum_view_per_hour = {}
-        cpt = 0
-        for raw in raws:
-            if cpt >= nb_pages:
-                break
-            clean_name = cleaner(raw['name'])
-            if clean_name is not None:
-                dict24hours_piechart.append({'category': clean_name, 'measure': int(raw['n_view'])})
-                for hour in range(24):
-                    hour_string = str(hour) if hour >= 10 else "0" + str(hour)
-                    dict24hours_linechart.append({'category': int(hour_string), 'group': clean_name, 'measure': int(raw['h' + hour_string])})
-                    if not 'h' + hour_string in sum_view_per_hour.keys():
-                        sum_view_per_hour['h' + hour_string] = int(raw['h' + hour_string])
-                    else:
-                        sum_view_per_hour['h' + hour_string] += int(raw['h' + hour_string])
-                sum_view += raw['n_view']
-                cpt += 1
+            if dict_piechart != []:
+                dict_piechart = [{'category': 'All', 'measure': sum_view}] + dict_piechart
 
-        dict24hours_piechart = [{'category': 'All', 'measure': sum_view}] + dict24hours_piechart
-        sorted_keys = sum_view_per_hour.keys()
-        sorted_keys.sort()
-        dict24hours_linechart = [{'category': int(key[1:]), 'group': 'All', 'measure': sum_view_per_hour[key]} for key in sorted_keys] + dict24hours_linechart
+                with open(os.path.join(application.root_path, path_to_data + 'data' + mode + '_piechart.csv'), 'wb') as f:
+                    w = csv.DictWriter(f, dict_piechart[0].keys())
+                    w.writeheader()
+                    for row in dict_piechart:
+                        w.writerow(row)
 
-        with open(os.path.join(application.root_path, path_to_data + 'data24hours_piechart.csv'), 'wb') as f:
-            w = csv.DictWriter(f, dict24hours_piechart[0].keys())
-            w.writeheader()
-            for row in dict24hours_piechart:
-                w.writerow(row)
+            if dict_linechart != []:
+                sorted_keys = sum_view_per_hour.keys()
+                sorted_keys.sort()
+                dict_linechart = [{'category': int(key[1:]), 'group': 'All', 'measure': sum_view_per_hour[key]} for key in sorted_keys] + dict_linechart
 
-        with open(os.path.join(application.root_path, path_to_data + 'data24hours_linechart.csv'), 'wb') as f:
-            w = csv.DictWriter(f, dict24hours_linechart[0].keys())
-            w.writeheader()
-            for row in dict24hours_linechart:
-                w.writerow(row)
+                with open(os.path.join(application.root_path, path_to_data + 'data' + mode + '_linechart.csv'), 'wb') as f:
+                    print dict_linechart
+                    w = csv.DictWriter(f, dict_linechart[0].keys())
+                    w.writeheader()
+                    for row in dict_linechart:
+                        if mode == '30days' and row['measure'] == 0:
+                            continue
+                        w.writerow(row)
 
-    ############
-    ## 30 days
-    ############
-    # Compute all dates
-        # from datetime import datetime, timedelta
-        # import pandas as pd
-        # date_min = datetime.strptime('2011-01-01', '%Y-%m-%d')
-        # date_max = datetime.strptime(date_string, '%Y-%m-%d')
-        # date_minus30days = max(date_max - timedelta(days=30), date_min)
-        # delta = date_max - date_minus30days
-
-        # all_dates_string = '('
-        # for i in range(delta.days + 1):
-        #     all_dates_string += "'" + str(date_minus30days + timedelta(days=i))[:10] + "',"
-        # all_dates_string = all_dates_string[:-1] + ')'
-
-        # dict30days_piechart = []
-        # dict30days_linechart = []
-        # query = "SELECT * FROM wiki.trends_24hours WHERE date_time IN " + all_dates_string + \
-        #         " AND n_view > 100000;"
-        # df_30hours = pd.DataFrame()
-        # raws = session.execute(query)
-        # for raw in raws:
-        #     df_30hours = df_30hours.append(raw, ignore_index=True)
-        # df_30hours.groupby('name')['n_view'].sum().reset_index().sort_values('n_view', ascending=False)[:10]
-    return render_template('index.html')
+    return render_template('index.html', default_date=default_date)
 
 if __name__ == '__main__':
     application.run(debug=True)
